e646d18c2207bdcd76c9bdac6471e7a57fca344d
chrgjerd <chrgjerd@PCNO01031>
2017-04-14 17:39:30 +0200

    Updated default route

--- src/SimpleWebPage/App_Start/RouteConfig.cs
+++ src/SimpleWebPage/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SimpleWebPage
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "VMR", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
